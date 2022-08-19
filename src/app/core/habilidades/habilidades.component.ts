87432536fbadf2d7dacb37f9024bc2c6756ddde8
Danrleydev <danrleysouza53@gmail.com>
2022-08-19 14:48:25 -0300

    Finalização habilidades :rocket:

--- src/app/core/habilidades/habilidades.component.ts
+++ src/app/core/habilidades/habilidades.component.ts
@@ -23,17 +23,13 @@ export class HabilidadesComponent implements OnInit {
         } else {
           currentItem += 1;
         }
-
         if (currentItem >= maxItems) {
           currentItem = 0;
         }
-
         if (currentItem < 0) {
           currentItem = maxItems - 1;
         }
-
         items.forEach((item) => item.classList.remove("current-item"));
-
         items[currentItem].scrollIntoView({
           behavior: "smooth",
           inline: "center"
